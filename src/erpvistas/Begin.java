4cf66714b54f4fc0730125a3c5e90b1ad72baee2
nombre <jordiav0807@gmail.com>
2023-05-04 11:05:05 -0600

    Primer avance, login y base aportada por el profe

---
+++ src/erpvistas/Begin.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package erpvistas;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author ucr
+ */
+public class Begin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+//
+//        FRMPOS pos = new FRMPOS();
+//
+//        pos.setBounds(0, 0, ((int) width), ((int) height));
+//        pos.setVisible(true);
+//        pos.setLocationRelativeTo(null);
+        
+        Login lo = new Login();
+        lo.setVisible(true);
+    }
+
+}
